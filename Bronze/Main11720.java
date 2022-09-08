c0f7a45181907ce461b32a128da236f3e5c5871a
hellozo0 <ahju0011@gmail.com>
2022-09-08 14:36:10 +0900

    [B2] 숫자의 합 구하기_ 새로운 방법으로 구현

--- Bronze/Main11720.java
+++ Bronze/Main11720.java
@@ -1,19 +1,38 @@
 package Bronze;
 import java.util.*;
 
+// public class Main11720 {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+        
+//         int N = sc.nextInt();
+//         String letter = sc.next();
+//         sc.close();
+        
+//         int sum = 0;
+//         for (int i = 0; i < N; i++){
+//             sum += letter.charAt(i)-'0';
+//         }
+
+//         System.out.println(sum);
+//     }
+// }
+
+
 public class Main11720 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         int N = sc.nextInt();
-        String letter = sc.next();
-        sc.close();
-        
+
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+
         int sum = 0;
-        for (int i = 0; i < N; i++){
-            sum += letter.charAt(i)-'0';
+        for (int i = 0; i < N; i++) {
+            sum += cNum[i] - '0';
         }
 
         System.out.println(sum);
+
     }
 }
