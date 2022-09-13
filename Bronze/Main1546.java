ee02d864b2497f12bc53f4b88f0f52903f64b51a
hellozo0 <ahju0011@gmail.com>
2022-09-13 17:25:16 +0900

    최대 힙 리스트로 구현

--- Bronze/Main1546.java
+++ Bronze/Main1546.java
@@ -2,30 +2,56 @@ package Bronze;
 import java.util.*;
 
 
+// public class Main1546 {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int N = sc.nextInt();
+//         Double[] arr = new Double[N];
+//         Double M;
+//         Double sum = (double) 0;
+
+//         for (int i = 0; i < N; i++){
+//             arr[i] = sc.nextDouble();
+//         }
+
+//         M = arr[0];
+//         for (int j = 1; j < N; j++){
+//             if (M < arr[j]){
+//                 M = arr[j];
+//             }
+//         }
+
+//         for (int k = 0; k <N; k++){
+//             sum += (arr[k] / M) * 100;
+//         }
+//         System.out.printf("%.2f" ,(sum/N));
+
+//         sc.close();
+//     }  
+// }
+
+
+
 public class Main1546 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        Double[] arr = new Double[N];
-        Double M;
-        Double sum = (double) 0;
+        int[] score = new int[N];
 
-        for (int i = 0; i < N; i++){
-            arr[i] = sc.nextDouble();
+        for (int i = 0; i < N; i++) {
+            score[i] = sc.nextInt();
         }
 
-        M = arr[0];
-        for (int j = 1; j < N; j++){
-            if (M < arr[j]){
-                M = arr[j];
+        long sum = 0;
+        long M = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (M < score[i]) {
+                M = score[i];
             }
+            sum += score[i];
         }
 
-        for (int k = 0; k <N; k++){
-            sum += (arr[k] / M) * 100;
-        }
-        System.out.printf("%.2f" ,(sum/N));
-
-        sc.close();
-    }  
+        System.out.println(sum / M * 100 / N);
+    }
 }
