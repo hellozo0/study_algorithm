b2be5f66b5afc38498b0f05dcbdaf4a1c5a2fd7f
hellozo0 <ahju0011@gmail.com>
2022-10-09 16:31:12 +0900

    [B2] 2750. 수 정렬하기_selection sort

--- Bronze/Main2525.java
+++ Bronze/Main2525.java
@@ -6,6 +6,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// public class Main2525 {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+//         StringTokenizer st;
+//         st = new StringTokenizer(br.readLine(), " ");
+
+//         int H = Integer.parseInt(st.nextToken());
+//         int M = Integer.parseInt(st.nextToken());
+
+//         int plus = Integer.parseInt(br.readLine());
+//         M += plus;
+
+//         if (M >= 60) {
+//             H += M/60;
+//             M = M % 60;
+//         }
+//         if (H > 23) {
+//             H -= 24;
+//         }
+//         System.out.println(H + " " + M);
+
+//     }
+// }
+
+
+//Selection sort
 public class Main2525 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
