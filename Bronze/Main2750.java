75814f6638f7dfd3b96ee641bfb6e885e8006417
hellozo0 <ahju0011@gmail.com>
2022-10-09 16:40:45 +0900

    [B2] 2750. 수정렬하기1_insertion_sort

--- Bronze/Main2750.java
+++ Bronze/Main2750.java
@@ -34,6 +34,40 @@ import java.io.InputStreamReader;
     
 // }
 
+// public class Main2750 {
+//     public static void main(String[] args) throws IOException {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//         int N = Integer.parseInt(br.readLine());
+//         int[] array = new int[N];
+
+//         for (int i = 0; i < N; i++) {
+//             array[i] = Integer.parseInt(br.readLine());
+//         }
+
+//         //slection sort
+//         for(int i = 0; i < array.length ; i++) {
+            
+//             int min_index = i;
+//             //최소값 인덱스 찾기
+//             for (int j = i+1; j < array.length; j++) {
+//                 if (array[j] < array[min_index]) {
+//                     min_index = j;
+//                 }
+//             }
+
+//             int temp = array[min_index];  
+//             array[min_index] = array[i];
+//             array[i] = temp;
+
+//         }
+
+//         for (int i = 0; i < N; i++) {
+//             System.out.println(array[i]);
+//         }
+//     }
+// }
+
 public class Main2750 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,20 +79,18 @@ public class Main2750 {
             array[i] = Integer.parseInt(br.readLine());
         }
 
-        //slection sort
+        //insertion sort
         for(int i = 0; i < array.length ; i++) {
-            
-            int min_index = i;
-            //최소값 인덱스 찾기
-            for (int j = i+1; j < array.length; j++) {
-                if (array[j] < array[min_index]) {
-                    min_index = j;
-                }
+            int target = array[i];
+
+            int j = i-1;
+
+            while(j>=0 &&  target < array[j]) {
+                array[j+1] = array[j];
+                j--;
             }
 
-            int temp = array[min_index];  
-            array[min_index] = array[i];
-            array[i] = temp;
+            array[j+1] = target;
 
         }
 
