932e433e35c35ea176f689a919df48933fe9e9f9
hellozo0 <ahju0011@gmail.com>
2022-10-27 13:57:29 +0900

    [B4] 11720.숫자의 합_BufferReader 사용

--- Bronze/Main11720.java
+++ Bronze/Main11720.java
@@ -1,4 +1,7 @@
 package Bronze;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 // public class Main11720 {
@@ -19,17 +22,33 @@ import java.util.*;
 // }
 
 
-public class Main11720 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+// public class Main11720 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int N = sc.nextInt();
 
-        String sNum = sc.next();
-        char[] cNum = sNum.toCharArray();
+//         String sNum = sc.next();
+//         char[] cNum = sNum.toCharArray();
+
+//         int sum = 0;
+//         for (int i = 0; i < N; i++) {
+//             sum += cNum[i] - '0';
+//         }
+
+//         System.out.println(sum);
+
+//     }
+// }
+
+public class Main11720 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
 
         int sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += cNum[i] - '0';
+
+        for(byte value : br.readLine().getBytes()){
+            sum += (value - '0');
         }
 
         System.out.println(sum);
