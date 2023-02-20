dac9af2b0a6b194b9aca9380a9dcda611cbac977
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2023-02-20 20:06:15 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
@@ -5,6 +5,7 @@
 long long solution(int a, int b) {
     long long answer = 0;
     
+    //반복문의 개수를 줄이는 방법이 최선의 방법이다 --> 처음 a < b 형태가 되도록 temp 써서 변환 한 후 1개의 for문 사용하기
     if ( b > a){
         for(int i = a; i <= b; i++){
             answer += i;
