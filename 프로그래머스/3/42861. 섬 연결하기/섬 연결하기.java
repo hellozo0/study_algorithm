3531a4d35741f23476d9ce00757683af0124b872
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-03-22 21:54:22 +0900

    [level 3] Title: 섬 연결하기, Time: 0.65 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,49 +2,46 @@ import java.util.*;
 
 class Solution {
     
-    static private int[] parent;
+    static int[] parents;
     
     public int solution(int n, int[][] costs) {
-        
-        //최소 비용 - 최소 신장 트리 ( 크루스칼, 프림 )
-        //(1) 크루스칼 : 모든 간선을 가중치 기준으로 오름차순 정렬 -> 이 간선들을 순서대로 모든 정점이 연결될때 까지 연결
-        // Union Find
-        //(2) 프림 : 트리 집합을 단계적으로 확장하는 것이고, 새로운 정점을 연결할때 마다 새로운 정점에서 갈 수 있는 아직 연결되지 않은 정점들에 대한 간선을 추가
-        // 우선순위 큐 사용
-        
         int answer = 0;
-        parent = new int[n];
+        parents = new int[n];
         
-        for(int i = 0; i < n; i++) {
-            parent[i] = i;
+        //0. parents 세팅
+        for(int i = 0; i < n; i++){
+            parents[i] = i;
         }
         
-        //가중치로 정렬 
-        // 011  131  022  125  238 순으로 정렬되겠지?
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
         
-        //크루스칼 
-        for(int i = 0; i < costs.length; i++) {
-            if(find(costs[i][0]) != find(costs[i][1])) { // 01 연결
-                union(costs[i][0], costs[i][1]);
-                answer += costs[i][2]; // 가중치 누적합
+        //1. 가중치 순으로 정렬
+        //011 131 022 125 238
+        Arrays.sort(costs, (o1,o2)-> o1[2] - o2[2]);
+        
+        for(int i = 0; i < costs.length; i++){
+            //2. 연결되어 있는지 확인 - 연결안되어 있다면 연결
+            if(find(costs[i][0]) != find(costs[i][1])){
+                //3. 연결 안되어 있으면 연결하고 answer++
+                union(costs[i][0], find(costs[i][1]));
+                answer += costs[i][2];         
             }
         }
+        
         return answer;
     }
     
-    //a가 어떤 집합에 포함되어 있는지 찾는 연산
-    public int find(int a) {
-        if(parent[a] == a) return a;
-        else return parent[a] = find(parent[a]);
-    }
-    
-    //a와 b가 포함되어 있는 집합을 합치는 연산
-    public void union(int a, int b) {
+    //union : 둘을 연결
+    private void union(int a, int b){
         a = find(a);
         b = find(b);
-        if(a != b) {
-            parent[b] = a; // [i][1] 을 세팅
+        if( a != b){
+            parents[b] = a;
         }
+    }
+    
+    //최상위 부모 헤드 찾기
+    private int find(int a){
+        if(parents[a] == a) return a; //기초 세팅 상태랑 같으면 그거 return
+        else return parents[a] = find(parents[a]); // 그래서 결국은 최상위 헤드 찾기
     }
 }
