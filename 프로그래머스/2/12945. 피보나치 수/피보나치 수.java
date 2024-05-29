d0198acda0cfa65b14841fd329ad344caa57a687
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-05-30 00:12:25 +0900

    [level 2] Title: 피보나치 수, Time: 2.88 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        
+        int[] fibo = new int[n+1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            fibo[i] = fibo[i-2] %1234567 + fibo[i-1] %1234567;
+        }
+        
+        
+        
+        return fibo[n] %1234567;
+    }
+    
+}
