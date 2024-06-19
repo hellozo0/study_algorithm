00b5abe1c40bf97c562f67cc19ec51f299f73af6
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-06-19 12:58:18 +0900

    [level 2] Title: 구명보트, Time: 8.93 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,19 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-
+        
         Arrays.sort(people);
-
-        int min = 0;
-
-        //앞에서 부터 세트를 묶는다는 생각X, min, max를 세트로 묶어서 처리하면 더 효율적
-        //뒤에서 부터 시작
-        //50(min) 50 70 80(max)
-        for (int max = people.length - 1; min <= max; max--){
-            if (people[min] + people[max] <= limit) min++;
+        
+        int start = 0; 
+        
+        
+        for(int end = people.length-1; end >= start; end--){
+            if(people[start] + people[end] <= limit) start++;
             answer++;
         }
-
+        
         return answer;
     }
 }
