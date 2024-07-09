5469cc73751cac771ce6005e78b0bfd46babdbb5
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-07-09 13:30:00 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,26 +1,21 @@
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        
         int answer = 1;
         
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         
         for(int i = 0; i < clothes.length; i++){
-            if(map.containsKey(clothes[i][1])){
+            if(map.containsKey(clothes[i][1])){ //개수 추가
                 map.replace(clothes[i][1], map.get(clothes[i][1])+1);
             } else {
                 map.put(clothes[i][1], 1);
             }
-        }
+        }   
         
-        //종류별로 옷 개수 map ex - 모자,2 & 상의,2 & 바지 1
-        Iterator<String> keys = map.keySet().iterator();
-        while(keys.hasNext()){
-            String key = keys.next();
-            answer *= (map.get(key)+1);
+        for(int i : map.values()){
+            answer = answer * (i+1);
         }
         
         return answer-1;
