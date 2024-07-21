2bade55bb2c3efde8c79fb1b3cb79b3f99562a30
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-07-21 13:10:51 +0900

    [level 3] Title: 이중우선순위큐, Time: 1.01 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -4,44 +4,31 @@ class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
         
-        PriorityQueue<Integer> minPQ = new PriorityQueue<>(); //최소힙
-        PriorityQueue<Integer> maxPQ = new PriorityQueue<>((o1,o2)-> o2 - o1); //최대힙
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>(); 
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder());
         
         for(int i = 0; i < operations.length; i++){
             StringTokenizer st = new StringTokenizer(operations[i]);
             String operation = st.nextToken();
             int num = Integer.parseInt(st.nextToken());
             
-            System.out.println("operation : " + operation + "   num : " + num );
-            
-            //큐가 비어있는데 D(삭제 연산)일 경우 무시
-            if(minPQ.size() < 1 && operation.equals("D")){
-                System.out.println("111111");
-                continue;
-            }
-            
             if(operation.equals("I")){
-                System.out.println("222222");
-                minPQ.add(num); //큐에 다 넣는다
-                maxPQ.add(num); //큐에 다 넣는다
+                minPQ.add(num); 
+                maxPQ.add(num); 
             } 
-            else if(operation.equals("D") && num == 1){ //최대값 삭제
-                System.out.println("33333");
+            else if(operation.equals("D") && num == 1 && !maxPQ.isEmpty()){
                 int del = maxPQ.poll();
-                minPQ.remove(del); //minPQ에서도 해당 숫자 삭제
-            } else if (operation.equals("D") && num == -1){
-                System.out.println("44444");
+                minPQ.remove(del); 
+            } else if (operation.equals("D") && num == -1 && !maxPQ.isEmpty()){
                 int del = minPQ.poll();
-                maxPQ.remove(del); //minPQ에서도 해당 숫자 삭제
+                maxPQ.remove(del); 
             }
 
         }        
         if(minPQ.isEmpty()){
-            answer[0] = 0;
-            answer[1] = 0;
+            answer = new int[] {0,0};
         } else {
-            answer[0] = maxPQ.poll();
-            answer[1] = minPQ.poll();
+            answer = new int[] {maxPQ.poll(), minPQ.poll()};
         }
             
         return answer;
