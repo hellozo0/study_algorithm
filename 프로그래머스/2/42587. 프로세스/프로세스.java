d3c0ced467067f75d32ee27ca4b180d1dce5c26f
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-07-29 21:13:54 +0900

    [level 2] Title: 프로세스, Time: 3.54 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,27 +1,36 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
-        //우선순위큐 문제 3221 ,  911111 저장 될것이다.
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
-        for(int i = 0; i < priorities.length; i++){
-            q.add(priorities[i]);
+        // pq.add(0);
+        // pq.add(1);
+        // pq.add(2);
+        // System.out.println(pq.peek());
+        
+        for(int i = 0; i < priorities.length; i++) {
+            pq.add(priorities[i]);
         }
         
-        while (!q.isEmpty()) { //3221일때 , 911111
-            for (int i = 0; i < priorities.length; i++) { //index0부터 비교 
-                if (q.peek() == priorities[i]) { //i = 2일때 , i = 2일때,3,4,5,0
-                    q.poll(); //3을 큐에서 뺀다, 9을 뺀다
-                    answer++; // answer = 1이 되고, answer = 1,,2,3,4,5
-
-                    if (i == location) { //location이랑 i랑같을때 
-                        return answer; // answer = 1 , 5 리턴
-                    }
+        
+        while(!pq.isEmpty()) { // 2번쨰 while 돌때 
+            for(int i = 0; i < priorities.length; i++) {
+                //i = 3 일때 0[3]1[4]1[5]1[6], 1[0]1[1]
+                if(pq.peek() == priorities[i]) {
+                    pq.poll();
+                    answer++; //1-> 2-> 3-> 4
+                    
+                    if( i == location ) return answer;
                 }
             }
         }
+        
+        
+        
+        
+        
+        
         
         return answer;
     }
