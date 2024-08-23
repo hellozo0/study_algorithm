5300e3ae3c22b59650b50a157a701675ec5a4a88
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-08-23 12:58:59 +0900

    [level 3] Title: 등굣길, Time: 1.03 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,33 +1,34 @@
 import java.util.*;
 
 class Solution {
+    
     public int solution(int m, int n, int[][] puddles) {
-        int mod = 1000000007;
+     
+        int[][] maps = new int[n+1][m+1];
+        final int mod = 1000000007;
         
-        int[][] way = new int[n+1][m+1];
-        
-        for(int[] puddle : puddles){
-            way[puddle[1]][puddle[0]] = -1;
+        for(int i = 0; i < puddles.length; i++) {
+            maps[puddles[i][1]][puddles[i][0]] = -1;
         }
-        way[1][1] = 1;
-        for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < m+1; j++){
-                //1. 웅덩이 일 경우 continue;
-                if( way[i][j] == -1) {
-                    continue;
+        
+        maps[1][1] = 1;
+        
+        for(int i = 1; i <=n; i++){
+            for(int j = 1; j <=m; j++){
+                if(maps[i][j] == -1) continue;
+                
+                if(maps[i-1][j] > 0) {
+                    maps[i][j] += maps[i-1][j] % mod;
                 }
-                //왼쪽이 웅덩이일 경우 i j 기준
-                if( way[i-1][j] > 0) {
-                    way[i][j] += way[i-1][j] % mod;
-                }
-                //위에가 웅덩이일 경우 i j 기준
-                if( way[i][j-1] > 0) {
-                    way[i][j] += way[i][j-1] % mod;
+                
+                if(maps[i][j-1] > 0){
+                    maps[i][j] += maps[i][j-1] % mod;
                 }
                 
             }
         }
         
-        return way[n][m] % mod;
+        return maps[n][m] % mod;        
+        
     }
 }
