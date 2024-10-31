6c7deef10c8ca41842bbb7508f1324a30583259c
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-10-31 17:23:04 +0900

    [Gold IV] Title: 운동, Time: 812 ms, Memory: 63520 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -8,9 +8,8 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    static ArrayList<Edge>[] list;
+    static int INF = 9999999;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,44 +17,33 @@ public class Main {
         int E = Integer.parseInt(st.nextToken());
 
         //1. 자료구조 선언
-        list = new ArrayList[V+1];
         int[][] distance = new int[V+1][V+1];
-        boolean[][] visited = new boolean[V+1][V+1];
-
 
         //2. 초기화
         for(int i = 1; i < V+1; i++){
             for(int j = 1; j < V+1; j++){
-                distance[i][j] = 9999999;
+                distance[i][j] = INF;
             }
         }
-        for(int i = 1; i < V+1; i++){
-            list[i] = new ArrayList<>();
-        }
-
+        
         //3. 입력받기
         for(int i = 0; i <E; i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            list[u].add(new Edge(w,v));
+            distance[u][w] = v;
         }
 
-        //4. 다익스트라
-        for(int i = 1; i < V+1; i++) {
-            dijkstra(i, distance, visited);
-        }
-
-        //5. 플로이드 워셜
+        //4. 플로이드 워셜
         floyd(V, distance);
 
-        int min = 9999999;
+        int min = INF;
         for(int i = 1; i < V+1; i++){
             min = Math.min(min, distance[i][i]);
         }
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(String.valueOf(min == 9999999?-1:min));
+        bw.write(String.valueOf(min == INF?-1:min));
 
         bw.flush();
         bw.close();
@@ -63,55 +51,17 @@ public class Main {
 
     }
 
-    private static void dijkstra(int start, int[][] distance, boolean[][] visited){
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        distance[start][start] = 0;
-        pq.add(new Edge(start, 0));
-
-        while (!pq.isEmpty()){
-            Edge now = pq.poll();
-
-            if(visited[start][now.vertex]) continue;
-            visited[start][now.vertex] = true;
-
-            for(Edge next : list[now.vertex]){
-                if(!visited[start][next.vertex] && distance[start][next.vertex] > distance[start][now.vertex] + next.value){
-                    distance[start][next.vertex] = distance[start][now.vertex] + next.value;
-                    pq.add(new Edge(next.vertex, distance[start][next.vertex]));
-                }
-            }
-        }
-    }
     private static void floyd(int V, int[][] distance){
-        for(int i = 1; i < V+1; i++){
-            distance[i][i] = 9999999;
-        }
-        // for(int i = 1; i < V+1; i++){
-        //     for(int k = 1; k < V+1; k++){
-        //         distance[i][i] = Math.min(distance[i][i], distance[i][k] + distance[k][i]);
-        //     }
-        // }
         for(int k = 1; k < V+1; k++){
             for(int i = 1; i < V+1; i++){
                 for(int j = 1; j < V+1; j++){
-                    distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                    if(distance[i][k] != INF && distance[k][j] != INF){
+                        distance[i][j] = Math.min(distance[i][j], distance[i][k] + distance[k][j]);
+                    }
                 }
             }
         }
     }
-}
 
-class Edge implements Comparable<Edge> {
-    int vertex;
-    int value;
 
-    Edge(int vertex, int value) {
-        this.vertex = vertex;
-        this.value = value;
-    }
-
-    @Override
-    public int compareTo(Edge e) {
-        return value - e.value;
-    }
 }
