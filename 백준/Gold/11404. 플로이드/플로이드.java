523b62221c0a776fe27dda00f26221da908d5010
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-10-31 15:34:16 +0900

    [Gold IV] Title: 플로이드, Time: 432 ms, Memory: 43492 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,102 +1,68 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+public class Main{
 
-public class Main {
-    static ArrayList<Edge>[] list;
+    static int[][] graph;
+    static int n;
+    static final int max = 999999999;
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st; 
-        int N = Integer.parseInt(br.readLine()); //5
-        int M = Integer.parseInt(br.readLine()); //14
+        StringTokenizer st;
 
-        list = new ArrayList[N+1];
-        for(int i = 1; i < N+1; i++) { //1~N까지
-            list[i] = new ArrayList<>();
-        }
+        n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        graph = new int[n+2][n+2];
 
-        //1. 자료구조
-        long[][] distance = new long[N+1][N+1];
-        boolean[][] visited = new boolean[N+1][N+1];
-
-        //2. 초기화
-        for(int i = 1; i < N+1; i++){ //1~N까지
-            for(int j = 1; j < N+1; j++) {
-                distance[i][j] = Long.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(i==j) continue;
+                graph[i][j] = max;
             }
         }
 
-        for (int i = 0; i < M; i++){
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            list[u].add(new Edge(w,v));
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+
+            //같은 노선에 여러 가지가 올 수 있음 아래 부분에서 틀린것 같음
+            //graph[a][b] = value;
+            graph[a][b] = Math.min(value, graph[a][b]);
         }
 
-        for(int i = 1; i < N+1; i++){
-            dijkstra(i,distance, visited);
-        }
 
+        floyd();
+        br.close();
+
+    }
+
+    private static void floyd() throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        for(int i = 1; i < N+1; i++){
-            for(int j = 1; j < N+1; j++){
-                if(distance[i][j] == Long.MAX_VALUE) {
-                    bw.append("0 ");
-                } else {
-                    bw.append(String.valueOf(distance[i][j])).append(" ");
+        for (int k = 1; k <= n; k++) { //중간 노드
+            for (int i = 1; i <= n; i++) { //시작
+                for (int j = 1; j <= n; j++) { //도착
+                    graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
                 }
             }
-            if(i != N) bw.append("\n");
+        }
+
+        for(int i = 1; i <=n; i++){
+            for(int j = 1; j <=n; j++){
+                if(graph[i][j] == max) bw.append("0 ");
+                else bw.append(String.valueOf(graph[i][j])).append(" ");
+            }
+            if(i != n) bw.append("\n");
         }
         bw.flush();
         bw.close();
-        br.close();
-
-
-        //나중에 반복문 돌면서 Long.MAX_VALUE이면 0으로 넣어서 출력해야함
-    }
-
-    private static void dijkstra(int start, long[][] distance, boolean[][] visited){
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        distance[start][start] = 0;
-        pq.add(new Edge(start,0));
-
-        while (!pq.isEmpty()){
-            Edge now = pq.poll();
-
-            if (visited[start][now.vertex]) continue;
-            visited[start][now.vertex] = true;
-
-            for(Edge next : list[now.vertex]){
-                if(!visited[start][next.vertex] && distance[start][next.vertex] > distance[start][now.vertex] + next.value){
-                    distance[start][next.vertex] = distance[start][now.vertex] + next.value;
-                    pq.add(new Edge(next.vertex, distance[start][next.vertex]));
-                }
-            }
-        }
-
     }
 }
 
-class Edge implements Comparable<Edge> {
-    int vertex;
-    long value;
-
-    Edge(int vertex, long value) {
-        this.vertex = vertex;
-        this.value = value;
-    }
-
-    @Override
-    public int compareTo(Edge e) {
-        return Long.compare(this.value, e.value);
-    }
-}
