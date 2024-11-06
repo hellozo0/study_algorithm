8249ef869ae41bdf819d78c0ba370fee83b615ee
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-11-06 22:21:16 +0900

    [Gold IV] Title: 발전소 설치, Time: 188 ms, Memory: 20200 KB -BaekjoonHub

--- 백준/Gold/1277. 발전소 설치/발전소 설치.java
+++ 백준/Gold/1277. 발전소 설치/발전소 설치.java
@@ -1,33 +1,40 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+
+//다시 풀어봄
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static int N, W;
     static double M, INF = 200001;
-    static Point[] plant;
+    static Edge[] edge;
     static boolean[][] connected;
 
     public static void main(String[] args) throws IOException {
-        String[] input = br.readLine().split(" ");
-        N = Integer.parseInt(input[0]);
-        W = Integer.parseInt(input[1]);
-        String MLine = br.readLine();
-        M = Double.parseDouble(MLine);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        M = Double.parseDouble(br.readLine()); //max 길이
 
-        plant = new Point[N + 1];
+        edge = new Edge[N + 1];
         connected = new boolean[N + 1][N + 1];
 
         for (int i = 1; i < N + 1; i++) {
-            String[] line = br.readLine().split(" ");
-            plant[i] = new Point(Integer.parseInt(line[0]), Integer.parseInt(line[1]));
+            st = new StringTokenizer(br.readLine());
+            edge[i] = new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            //00, 01, 11, 21, 22, 32, 33, 41, 43 좌표 넣음
         }
 
         for (int i = 0; i < W; i++) {
-            String[] line = br.readLine().split(" ");
-            connected[Integer.parseInt(line[0])][Integer.parseInt(line[1])] = true;
-            connected[Integer.parseInt(line[1])][Integer.parseInt(line[0])] = true;
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            connected[u][w] = true;
+            connected[w][u] = true;
         }
 
         System.out.println(dijstra());
@@ -35,25 +42,31 @@ public class Main {
 
     private static long dijstra() {
         double[] distance = new double[N + 1];
-        for (int i = 1; i < N + 1; i++) {
-            distance[i] = INF;
-        }
+        boolean[] visited = new boolean[N + 1];
+
+        //1. 초기화
+        Arrays.fill(distance, INF);
         distance[1] = 0;
+
+        //2. 연결되어 있는 발전소는 distance = 0
         for (int i = 2; i < N + 1; i++) {
             if (connected[1][i]) distance[i] = 0;
         }
 
-        boolean[] visited = new boolean[N + 1];
+        //3. 다익스트라
         for (int i = 0; i < N; i++) {
-            double minDist = INF;
-            int cur = 0;
+            double minDistance = INF; //최소거리
+            int cur = 0; //인덱스
+
             for (int j = 1; j < N + 1; j++) {
-                    if (!visited[j] && minDist >= distance[j]) {
-                        minDist = distance[j];
-                        cur = j;
-                    }
+                if (!visited[j] && minDistance >= distance[j]) {
+                    minDistance = distance[j];
+                    cur = j;
+                }
             }
+
             if (cur == N) break;
+
             visited[cur] = true;
             for (int j = 1; j < N + 1; j++) {
                 if (j == cur) continue;
@@ -69,19 +82,20 @@ public class Main {
     private static double getDistance(int cur, int next) {
         if (connected[cur][next]) return 0;
 
-        Point src = plant[cur];
-        Point dest = plant[next];
+        Edge src = edge[cur];
+        Edge dest = edge[next];
         double dist = Math.pow(src.x - dest.x, 2) + Math.pow(src.y - dest.y, 2);
-        return Math.sqrt(dist);
+        dist = Math.sqrt(dist); // 유클리드 거리 계산
+        return dist > M ? INF : dist;
     }
+}
 
-    static class Point {
-        int x;
-        int y;
+class Edge {
+    int x;
+    int y;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    public Edge(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
