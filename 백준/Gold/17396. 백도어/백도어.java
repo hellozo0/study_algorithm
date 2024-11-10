8ce01a4b5df993d3b91ad3b5461800fe795e8ecf
An HyeonJu <62981652+hellozo0@users.noreply.github.com>
2024-11-10 19:19:49 +0900

    [Gold V] Title: 백도어, Time: 956 ms, Memory: 146196 KB -BaekjoonHub

--- 백준/Gold/17396. 백도어/백도어.java
+++ 백준/Gold/17396. 백도어/백도어.java
@@ -3,9 +3,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -16,61 +16,57 @@ public class Main {
 
         //1. 자료구조 선언
         ArrayList<Edge>[] list = new ArrayList[N];
+        int[] scan = new int[N];
         long[] distance = new long[N];
-        boolean[] visited = new boolean[N];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        final long INF = Long.MAX_VALUE;
 
         //2. 초기화
-        for(int i = 0; i < N; i++){
-            list[i] = new ArrayList<>();
-            distance[i] = Long.MAX_VALUE;
-        }
-
-        //3. 입력
+        Arrays.fill(distance, INF);
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            int flag = Integer.parseInt(st.nextToken());
-            if(flag == 1 && i != N-1) {
-                visited[i] = true;
-            }
+        for(int i  = 0; i < N;i++){
+            scan[i] = Integer.parseInt(st.nextToken());
+            list[i] = new ArrayList<>();
         }
+        scan[N-1] = 0; //어차피 target
+
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            list[u].add(new Edge(w,v));
-            list[w].add(new Edge(u,v));
+            int w = Integer.parseInt(st.nextToken());
+            list[u].add(new Edge(v,w));
+            list[v].add(new Edge(u,w));
         }
 
-        //4. 다익스트라
-        pq.add(new Edge(0,0));
+        //3. 다익스트라
         distance[0] = 0L;
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(0,0));
 
         while (!pq.isEmpty()){
             Edge now = pq.poll();
-
-            if(visited[now.vertex]) continue;
-            visited[now.vertex] = true;
+            
+            if(scan[now.vertex] == 1) continue;
+            scan[now.vertex] = 1;
 
             for(Edge next : list[now.vertex]){
-                if(!visited[next.vertex] && distance[next.vertex] > distance[now.vertex] + next.value){
+                if(scan[next.vertex] == 0 && distance[next.vertex] > distance[now.vertex] + next.value){
                     distance[next.vertex] = distance[now.vertex] + next.value;
                     pq.add(new Edge(next.vertex, distance[next.vertex]));
                 }
             }
         }
 
-        System.out.println(distance[N-1] == Long.MAX_VALUE?-1:distance[N-1]);
+        //4. 출력
+        System.out.println(distance[N-1] != INF ? distance[N-1] : -1);
 
     }
 }
-
-class Edge implements Comparable<Edge> {
+class Edge implements Comparable<Edge>{
     int vertex;
     long value;
 
-    Edge(int vertex, long value) {
+    Edge(int vertex, long value){
         this.vertex = vertex;
         this.value = value;
     }
