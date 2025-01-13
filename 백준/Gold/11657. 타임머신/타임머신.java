2170b6405f6f512ec6c46eb0490a3a25f197f83a
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-01-13 22:58:39 +0900

    [Gold IV] Title: 타임머신, Time: 240 ms, Memory: 19480 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,86 +1,91 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
+        //[그래프 문제다] 다익스트라? ㄴㄴ 음수 가능한거 인지 해야하니까   ""벨만포드""
+        //N개 도시 <= 500
+        //M개 버스 <= 6000
+        //C가 0이면 순간이동, C <0 이면 타임머신 시간 되돌림
+
+        //만약 음수 사이클 있으면 -1출력,
+        //아니라면 1=> 2 ,3,4,... 를 출력하깅~
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        final int INF = 987654321;
 
-        // 1. 자료구조 선언
+        //1. 자료 구조 선언
+        final int INF = 999999999;
+        long[] distance = new long[N+1];
         Edge[] edges = new Edge[M];
-        long[] distance = new long[N + 1];
 
-        // 2. 초기화
+        //2. 초기화 & 입력
         Arrays.fill(distance, INF);
-        for (int i = 0; i < M; i++) {
+        distance[1] = 0;
+        for(int i = 0; i < M;i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(u, w, v);
+            edges[i] = new Edge(u,w,v);
         }
 
-        // 3. 벨만-포드 알고리즘
-        distance[1] = 0;
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                Edge edge = edges[j];
-                if (distance[edge.start] == INF) continue;
-                
-                if (distance[edge.end] > distance[edge.start] + edge.distance) {
-                    distance[edge.end] = distance[edge.start] + edge.distance;
+        //3. 벨만 포드 _ 시작점은 1
+        for(int i = 1; i < N; i++){
+            for(int j = 0; j < M; j++){//j개 간선 수 만큼
+                Edge now = edges[j];
+
+                if(distance[now.u] != INF && distance[now.w] > distance[now.u] + now.v) {
+                    distance[now.w] = distance[now.u] + now.v;
                 }
             }
         }
 
-        // 음수 사이클 확인
+        //4. 음수 사이클 탐색
         boolean cycle = false;
-        for (int i = 0; i < M; i++) {
-            Edge edge = edges[i];
-            if (distance[edge.start] != INF && distance[edge.end] > distance[edge.start] + edge.distance) {
-                cycle = true;
+        for(int j = 0 ; j < M; j++){
+            Edge now = edges[j];
+
+            if(distance[now.u] != INF && distance[now.w] > distance[now.u] + now.v) {
+                cycle = true; //이미 음수 사이클 있다는 말임
                 break;
             }
         }
 
-        // 결과 출력
-        StringBuilder sb = new StringBuilder();
-        if (cycle) {
-            sb.append("-1\n");  // 음수 사이클이 존재하면 -1 출력 후 종료
+        //5. 조건 _ 반복문 출력
+        StringBuilder sb = new StringBuilder();;
+        if(cycle) {
+            sb.append("-1");
         } else {
-            for (int i = 2; i <= N; i++) {
-                if (distance[i] == INF) {
-                    sb.append("-1\n");  // 도달 불가능한 경우 -1 출력
+            for(int i = 2; i <=N; i++){
+                if(distance[i] == INF) {
+                    sb.append("-1\n");
                 } else {
                     sb.append(distance[i]).append("\n");
                 }
             }
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb.toString());
+
     }
 }
 
-class Edge {
-    int start;
-    int end;
-    int distance;
+class Edge{
+    int u;
+    int w;
+    int v;
 
-    Edge(int start, int end, int distance) {
-        this.start = start;
-        this.end = end;
-        this.distance = distance;
+    Edge(int u, int w, int v) {
+        this.u = u;
+        this.w = w;
+        this.v = v;
     }
 }
