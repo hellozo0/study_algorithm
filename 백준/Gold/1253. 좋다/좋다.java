b1ee80007e14940d590755ce2f0f89459ad445a0
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-01-16 23:07:16 +0900

    [Gold IV] Title: 좋다, Time: 168 ms, Memory: 17292 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,50 +2,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int answer = 0;
-
         long[] A = new long[N];
-        for(int i = 0; i < N; i++){
+
+        for(int i = 0; i < N;i ++){
             A[i] = Long.parseLong(st.nextToken());
         }
-
         Arrays.sort(A);
+        int answer = 0;
 
-        for(int k = 0; k < N; k++){
-            long target = A[k];
-            int i = 0;
-            int j = N-1;
+        for(int i = 0; i < N; i++){
+            long target = A[i];
+            int start = 0;
+            int last = N-1;
 
-            while(i < j){
-                if( A[i] + A[j] == target){
-                    if(i != k && j != k){
+            while (start < last) {
+                if(A[start] + A[last] == target) {
+                    if(start != i && last != i) {
                         answer++;
                         break;
-                    } else if ( i == k){
-                        i++;
-                    } else if (j == k) {
-                        j--;
                     }
-                } else if (A[i] + A[j] < target) {
-                    i++;
+                    else if (start == i) {
+                        start++;
+                    }
+                    else if (last == i) {
+                        last--;
+                    }
+                } else if (A[start] + A[last] > target){
+                    last--;
                 } else {
-                    j--;
+                    start++;
                 }
             }
-
         }
-
         System.out.println(answer);
-
     }
 }
