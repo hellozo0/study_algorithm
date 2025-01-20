d9b831f374c3168ecd2e7660ac70b5906402071f
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-01-20 22:17:54 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 592 ms, Memory: 69396 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,117 +1,104 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
- 
-class Node implements Comparable<Node> {
-    int end;
-    int weight;
- 
-    Node(int end, int weight) {
-        this.end = end;
-        this.weight = weight;
-    }
- 
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
- 
-}
- 
+
 public class Main {
-    static int N, E;
-    static ArrayList<ArrayList<Node>> a; // 인접리스트.
-    static int[] dist; // 시작점에서 각 정점으로 가는 최단거리.
-    static boolean[] check; // 방문 확인.
+
+    static int N,E;
+    static boolean[] visited;
+    static int[] distance;
+    static ArrayList<Edge>[] list;
     static final int INF = 200000000;
- 
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
- 
-        a = new ArrayList<>();
-        dist = new int[N + 1];
-        check = new boolean[N + 1];
- 
-        Arrays.fill(dist, INF);
- 
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
+
+        int N = Integer.parseInt(st.nextToken()); //정점 개수
+        int E = Integer.parseInt(st.nextToken()); //간선 개수
+
+        //1. 자료구조 선언
+        visited = new boolean[N+1];
+        distance = new int[N+1];
+        list = new ArrayList[N+1];
+
+        //2. 초기화
+        for(int i = 1; i < N+1; i++) {
+            list[i] = new ArrayList<>();
         }
- 
-        // 양방향 인접 리스트 구현.
-        for (int i = 0; i < E; i++) {
+        for(int i = 0; i < E; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
- 
-            // start에서 end로 가는 weight (가중치)
-            a.get(start).add(new Node(end, weight));
- 
-            // end에서 start로 가는 weight (가중치)
-            a.get(end).add(new Node(start, weight));
+            int u = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list[u].add(new Edge(w,v));
+            list[w].add(new Edge(u,v));
         }
- 
-        // 반드시 거쳐야 하는 정점.
         st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
- 
-        // 1 -> v1 -> v2 -> N으로 가는 경우
-        int res1 = 0;
-        res1 += dijkstra(1, v1);
-        res1 += dijkstra(v1, v2);
-        res1 += dijkstra(v2, N);
- 
-        // 1 -> v2 -> v1 -> N으로 가는 경우
-        int res2 = 0;
-        res2 += dijkstra(1, v2);
-        res2 += dijkstra(v2, v1);
-        res2 += dijkstra(v1, N);
- 
-        int ans = (res1 >= INF && res2 >= INF) ? -1 : Math.min(res1, res2);
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        int node1 = Integer.parseInt(st.nextToken());
+        int node2 = Integer.parseInt(st.nextToken());
+
+        int distance1 = 0; // v1 -> v2
+        int distance2 = 0; // v2 -> v1
+
+        //3. 다익스트라 시작
+        //1 -> v1 -> v2 -> N
+        distance1 += dijkstra(1,node1);
+        distance1 += dijkstra(node1,node2);
+        distance1 += dijkstra(node2,N);
+
+        //1 -> v2 -> v1 -> N
+        distance2 += dijkstra(1,node2);
+        distance2 += dijkstra(node2,node1);
+        distance2 += dijkstra(node1,N);
+
+        System.out.println((distance1 >= INF && distance2 >= INF) ? -1 : Math.min(distance1,distance2));
+
     }
- 
-    // 다익스트라 알고리즘
-    public static int dijkstra(int start, int end) {
-        Arrays.fill(dist, INF);
-        Arrays.fill(check, false);
- 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        boolean[] check = new boolean[N + 1];
-        pq.offer(new Node(start, 0));
-        dist[start] = 0;
- 
-        while (!pq.isEmpty()) {
-            Node curNode = pq.poll();
-            int cur = curNode.end;
- 
-            if (!check[cur]) {
-                check[cur] = true;
- 
-                for (Node node : a.get(cur)) {
-                    if (!check[node.end] && dist[node.end] > dist[cur] + node.weight) {
-                        dist[node.end] = dist[cur] + node.weight;
-                        pq.add(new Node(node.end, dist[node.end]));
+
+    private static int dijkstra(int start, int end){
+        //1. 여러번 돌려야하니까 초기화
+        Arrays.fill(distance, INF);
+        Arrays.fill(visited, false);
+
+        //2. 필요한 자료구조
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(start, 0));
+        distance[start] = 0;
+
+        while(!pq.isEmpty()){
+            Edge now = pq.poll();
+
+            if(!visited[now.node]){
+                visited[now.node] = true;
+
+                for(Edge next : list[now.node]) {
+                    if(!visited[next.node] && distance[next.node] > distance[now.node] + next.value){
+                        distance[next.node] = distance[now.node] + next.value;;
+                        pq.add(new Edge(next.node, distance[next.node]));
                     }
                 }
             }
         }
- 
-        return dist[end];
+        return distance[end];
+    }
+}
+
+class Edge implements Comparable<Edge>{
+    int node;
+    int value;
+
+    Edge(int node, int value){
+        this.node = node;
+        this.value = value;
+    }
+
+    @Override
+    public int compareTo(Edge e){
+        return this.value - e.value;
     }
 }
