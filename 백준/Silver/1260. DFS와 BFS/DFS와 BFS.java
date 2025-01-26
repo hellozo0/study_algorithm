1d085a1674aa1838bfb81e2cf345bf71154d9de6
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-01-26 16:11:30 +0900

    [Silver II] Title: DFS와 BFS, Time: 224 ms, Memory: 20376 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,108 +1,122 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
-import java.util.*;
+public class Main {
 
-public class Main{
-    
-    static int N,M;
-    static ArrayList<Integer>[] nodeList;
-    static boolean[] isVisted;
+	static boolean[] visited;
+	// static ArrayList<Integer>[] array;
+	static boolean[][] array2;
+    static int N;
+	static StringBuilder sb = new StringBuilder();
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
 
-    public static void dfs(int V) {
-        if(isVisted[V]) return;
+		//1. 자료구조 초기화
+		visited = new boolean[N + 1];
+		array2 = new boolean[N + 1][N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+            array2[u][w] = true;
+            array2[w][u] = true;
 
-        System.out.print(V + " ");
-        isVisted[V] = true;
+		}
 
-        //해당 노드에 존재하는 노드 수 만큼 반복
-        for(int i = 0; i< nodeList[V].size();i++){
-            int MIN = 1001;
+		// array = new ArrayList[N+1];
+		// for(int i = 1; i < N+1; i++){
+		//     array[i] = new ArrayList<>();
+		// }
+		// for(int i = 0; i < M; i++){
+		//     st = new StringTokenizer(br.readLine());
+		//     int u = Integer.parseInt(st.nextToken());
+		//     int w = Integer.parseInt(st.nextToken());
+		//     array[u].add(w);
+		// }
 
-            //  헤당 노드가 가장 낮은 수 인지 모두 방문해서 검증
-            for(int j = 0; j < nodeList[V].size(); j++){
-                int cur  = nodeList[V].get(j);
+		//2. DFS
+		dfs(start);
+		sb.append("\n");
+		// dfs_stack(start);
 
-                if(!isVisted[cur]) {
-                    MIN = Math.min(MIN,cur);
+		//3. BFS
+		Arrays.fill(visited, false);
+		bfs(start);
+		System.out.println(sb);
+
+	}
+
+	public static void dfs(int start) {
+
+		visited[start] = true;
+		sb.append(start).append(" ");
+
+        for(int i = 1; i <= N; i++){
+            if(array2[start][i]&& !visited[i]){
+                dfs(i);
+            }
+        }
+
+		// for (int node : array[start]) {
+		// 	if (!visited[node]) {
+		// 		dfs(node);
+		// 	}
+		// }
+	}
+
+	// public static void dfs_stack(int start) {
+    //
+	// 	Stack<Integer> stack = new Stack<>();
+	// 	stack.push(start);
+	// 	visited[start] = true;
+    //
+	// 	while (!stack.isEmpty()) {
+	// 		int now = stack.pop();
+	// 		sb.append(now + " ");
+    //
+	// 		for (int node : array[now]) {
+	// 			if (!visited[node]) {
+	// 				stack.push(node);
+	// 				visited[node] = true;
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	public static void bfs(int start) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(start);
+		visited[start] = true;
+
+		while (!queue.isEmpty()) {
+			int now = queue.poll();
+			sb.append(now).append(" ");
+
+            for(int i = 1; i <=N; i++){
+                if(array2[now][i] && !visited[i]){
+                    queue.add(i);
+                    visited[i] = true;
                 }
             }
-
-            if(MIN == 1001) break;
-            dfs(MIN);
-        }
-    }
-
-    private static void bfs(int V) {
-        Queue<Integer> q = new LinkedList<>();
-
-        q.add(V);
-        isVisted[V] = true;
-
-        while(!q.isEmpty()) {
-            int node = q.poll();
-            System.out.print(node + " ");
-
-            // 해당 노드에 존재하는 노드 수만큼 반복
-            for (int i = 0; i < nodeList[node].size(); i++) {
-                int MIN = 1001;
-
-                // 해당 노드가 가장 낮은 수인지 모두 방문하여 검증
-                for (int j = 0; j < nodeList[node].size(); j++) {
-                    int cur = nodeList[node].get(j);
-
-                    if(!isVisted[cur]) {
-                        MIN = Math.min(MIN, cur);
-                    }
-
-                }
-
-                if(MIN == 1001) break;
-
-                q.add(MIN);
-                isVisted[MIN] = true;
-            }
-
-        }
-
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        
-        N = Integer.parseInt(st.nextToken()); 
-        M = Integer.parseInt(st.nextToken()); 
-        int V = Integer.parseInt(st.nextToken()); 
-
-        nodeList = new ArrayList[N+1]; //정점의 수대로 배열 생성
-        isVisted = new boolean[N+1]; //방문 여부 판단
-
-        //간선 리스트 초기화
-        //1번 정점부터 리스트를 만들어서 배열에 넣기
-        for(int i = 1; i <= N; i++) {
-            nodeList[i] = new ArrayList<>();
-        }
-
-        //간선수 대로 반복하면서 연결리스트 생성
-        for(int i = 0; i < M; i++) {
-            StringTokenizer st2 = new StringTokenizer(br.readLine()," ");
-            int from = Integer.parseInt(st2.nextToken()); 
-            int to = Integer.parseInt(st2.nextToken()); 
-
-            //양방향 
-            nodeList[from].add(to);
-            nodeList[to].add(from);
-        }
-
-        dfs(V);
-
-        Arrays.fill(isVisted,false);
-        System.out.println();
-
-        bfs(V);
-
-    }
+			// for (int node : array[now]) {
+			// 	if (!visited[node]) {
+			// 		queue.add(node);
+			// 		visited[node] = true;
+			// 	}
+			// }
+		}
+	}
 }
