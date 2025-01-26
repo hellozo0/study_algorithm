8843b0657cb7182fdde3c4bafda85eff8724a5b7
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-01-27 02:41:06 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -5,78 +5,74 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main{
+public class Main {
 
-    private static boolean[][] visited;
-    private static int[][] maps;
-    private static int[] dx = {0,1,0,-1};
-    private static int[] dy = {1,0,-1,0};
-    static List<Integer> arr;
-    private static int N;
-    static int c;
+    static int[] dx = new int[]{1,0,-1,0}; //우,하,좌,상
+    static int[] dy = new int[]{0,1,0,-1};
+    static int[][] map;
+    static boolean[][] visited;
+    static int N;
+    static ArrayList<Integer> answer = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        maps = new int[N][N];
-        visited = new boolean[N][N];
-        arr = new ArrayList<>();
+        map = new int[N+1][N+1];
+        visited = new boolean[N+1][N+1];
 
-        for(int i = 0; i < N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String s = st.nextToken();
-            for(int j = 0; j < N; j++) {
-                maps[i][j] = s.charAt(j)-'0';
+        //1. 입력받기
+        for(int i = 1; i <=N; i++){
+            String line = br.readLine();
+            for(int j = 1; j <=N; j++){
+                map[i][j] = line.charAt(j-1) - '0';
             }
         }
 
-        c = 1;
-        int cnt = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                if(!visited[i][j] && maps[i][j] == 1){
-                    cnt++;
-                    dfs(i,j);
-                    arr.add(c);
-                    c = 1;
+        //2. BFS로 2중 포문으로 요소 하나하나 다 찾아볼까..?
+        //BFS로 누적합 하다가 다 끝나면 ArrayList에 넣고
+        for(int i = 1; i <=N; i++){
+            for(int j = 1; j <=N; j++){
+                if(!visited[i][j] && map[i][j] == 1) { //1이구 방문안했을때 bfs 시작
+                    bfs(i,j);
                 }
             }
         }
 
-        System.out.println(cnt);
-        Collections.sort(arr);
-
-        for(int i = 0; i < arr.size(); i++){
-            if(arr.get(i) != 0){
-                System.out.println(arr.get(i));
-            }
+        //3. 출력
+        System.out.println(answer.size());
+        Collections.sort(answer);
+        for(int i = 0; i < answer.size(); i++){
+            System.out.println(answer.get(i));
         }
-
 
     }
 
-    private static void dfs(int i, int j){
-        visited[i][j] = true;
-        Stack<int[]> stack = new Stack<>();
-        stack.push(new int[]{i,j});
+    private static void bfs(int x, int y){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x,y});
+        visited[x][y] = true;
+        int cnt = 1;
 
-        while (!stack.isEmpty()){
-            int[] now = stack.pop();
-            for(int k = 0; k < 4; k++){
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
+        while(!queue.isEmpty()){
+            int[] now = queue.poll(); //1이니까 들어왔겠지?
 
-                if( x >= 0&& y>=0 && x < N && y < N && !visited[x][y] && maps[x][y] == 1){
-                    visited[x][y] = true;
-                    stack.push(new int[]{x,y});
-                    c++;
+            //4방향 체크해야함 => 1인지 0인 누적합 할거니까
+            for(int i = 0; i < 4; i++){
+                int nextX = now[0] + dx[i];
+                int nextY = now[1] + dy[i];
+
+                if(nextX >= 1 && nextX <= N && nextY >= 1 && nextY <= N && map[nextX][nextY] == 1 && !visited[nextX][nextY]){
+                    queue.add(new int[]{nextX,nextY});
+                    visited[nextX][nextY] = true;
+                    cnt++;
                 }
+
             }
         }
+        answer.add(cnt);
     }
 
 }
