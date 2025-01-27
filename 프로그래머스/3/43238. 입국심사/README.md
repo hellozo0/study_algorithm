a9cd6daa4ea6fce5012d879d8249e206d63de1c1
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-01-27 15:02:12 +0900

    [level 3] Title: 입국심사, Time: 103.67 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.56 ms
+메모리: 87.1 MB, 시간: 103.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 15일 23:28:29
+2025년 01월 27일 15:02:09
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,28 +3,33 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
+        
+        
+        //가장 적은 시간과 가장 긴 시간을 구하고 mid 값을 조절해서 가능한지 여부를 파악한다
+        //1. low,high 세팅
         Arrays.sort(times);
-        long start = 1;
-    	long end = (long) times[times.length-1] * n; 
-
-    	while(start <= end) {
-    		long sum = 0;
-    		long mid = (start + end) / 2;
-
-    		for(int time : times) {
-    			sum += mid / time;
-    		}
-
-    		if(sum >= n) {
-				answer = mid;
-				end = mid - 1;
-    		}
-    		else {
-    			start = mid + 1;
-    		}
-    	}
-
-
+        long low = 1;
+        long high = n * (long)times[times.length-1];
+        
+        
+        //1 ~ 60  ==> 30으로 가능한지 
+        while(low <= high) {
+            long mid = (low + high) /2; //30 
+            long sum = 0;
+            
+            for(int time : times){
+                sum += mid / time; //4, 3 => 7개
+            }
+            
+            if(sum >= n) {
+                answer = mid;
+                high = mid-1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        
         return answer;
     }
 }
