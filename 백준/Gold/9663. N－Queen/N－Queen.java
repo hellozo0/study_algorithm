80a5b5770dc98c2f7ebb719b5342a493ab725e2b
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-02-05 09:45:04 +0900

    [Gold IV] Title: N-Queen, Time: 5868 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,24 +1,20 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     public static int[] arr;
     public static int N;
     public static int count = 0;
 
-    //문제를 어떻게 해결해 나가야하는지 이해 X
+    //문제를 어떻게 해결해 나가야하는지 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N];
 
         nQueen(0);
         System.out.println(count);
-
     }
 
     public static void nQueen(int depth) {
@@ -39,13 +35,11 @@ public class Main {
     }
 
     public static boolean Possibility(int col) {
-
         for (int i = 0; i < col; i++) {
             // 해당 열의 행과 i열의 행이 일치할경우 (같은 행에 존재할 경우)
             if (arr[col] == arr[i]) {
                 return false;
             }
-
             /*
              * 대각선상에 놓여있는 경우
              * (열의 차와 행의 차가 같을 경우가 대각선에 놓여있는 경우다)
@@ -54,7 +48,6 @@ public class Main {
                 return false;
             }
         }
-
         return true;
     }
 }
