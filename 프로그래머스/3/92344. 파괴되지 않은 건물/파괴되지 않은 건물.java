2154605591888da7c2e9b122aa232e28bcc47843
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-02-17 11:25:31 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 58.48 ms, Memory: 213 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,43 @@
+class Solution {
+    static int[][] sum;
+    static int N, M;
+    public static int solution(int[][] board, int[][] skill) {
+        N = board.length;
+        M = board[0].length;
+ 
+        sum = new int[N + 1][M + 1];
+        for (int[] s : skill) {
+            int y1 = s[1], x1 = s[2];
+            int y2 = s[3], x2 = s[4];
+            int degree = s[5] * (s[0] == 1 ? -1 : 1);
+ 
+            sum[y1][x1] += degree;
+            sum[y1][x2 + 1] += (degree * -1);
+            sum[y2 + 1][x1] += (degree * -1);
+            sum[y2 + 1][x2 + 1] += degree;
+        }
+        calculate();
+ 
+        //정답 계산
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] + sum[i][j] > 0) answer++;
+            }
+        }
+        return answer;
+    }
+ 
+    private static void calculate() {
+        for (int y = 1; y < N; y++) {
+            for (int x = 0; x < M; x++) {
+                sum[y][x] += sum[y - 1][x];
+            }
+        }
+        for (int x = 1; x < M; x++) {
+            for (int y = 0; y < N; y++) {
+                sum[y][x] += sum[y][x - 1];
+            }
+        }
+    }
+}
