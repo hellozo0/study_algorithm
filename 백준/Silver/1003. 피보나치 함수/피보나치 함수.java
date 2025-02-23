b735325026991eb923651783a3530f1c1fac2515
HyunJu An <62981652+hellozo0@users.noreply.github.com>
2025-02-23 21:48:25 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 16076 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,38 +1,37 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
+import java.io.InputStreamReader;
+
 public class Main {
- 
-	static Integer[][] dp = new Integer[41][2];
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		dp[0][0] = 1;	// N=0 일 때의 0 호출 횟수
-		dp[0][1] = 0;	// N=0 일 때의 1 호출 횟수
-		dp[1][0] = 0;	// N=1 일 때의 0 호출 횟수
-		dp[1][1] = 1;	// N=1 일 때의 1 호출 횟수
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		StringBuilder sb = new StringBuilder();
-		
-		while(T --> 0){
-			int N = Integer.parseInt(br.readLine());
-			fibonacci(N);
-			sb.append(dp[N][0] + " " + dp[N][1]).append('\n');
-		}
-		System.out.println(sb);
-	}
-	
-	static Integer[] fibonacci(int N) {
-		if(dp[N][0] == null || dp[N][1] == null) {
-			dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
-			dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
-		}
-		return dp[N];
- 
-	}
- 
+
+    static Integer[][] arrayFibo = new Integer[41][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        //호출횟수 ( 0일때 0호출 횟수 )
+        arrayFibo[0][0] = 1;
+        arrayFibo[0][1] = 0;
+        arrayFibo[1][0] = 0;
+        arrayFibo[1][1] = 1;
+
+        for(int i = 0; i < T; i++){
+            int N = Integer.parseInt(br.readLine());
+            checkFibo(N);
+            System.out.println(arrayFibo[N][0] + " " + arrayFibo[N][1]);
+        }
+
+    }
+
+    public static Integer[] checkFibo(int n){
+
+        if(arrayFibo[n][0] == null || arrayFibo[n][1] == null){
+            arrayFibo[n][0] = checkFibo(n-1)[0] + checkFibo(n-2)[0];
+            arrayFibo[n][1] = checkFibo(n-1)[1] + checkFibo(n-2)[1];
+        }
+
+        return arrayFibo[n];
+    }
+
 }
